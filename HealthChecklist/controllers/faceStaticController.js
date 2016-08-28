0902fdc70d9aa7094896ca5ccafa9b72e579a44d
Zhangzz72 <zhangzengzi@sina.com>
2016-08-29 00:19:33 +0800

    Bug fix

--- HealthChecklist/controllers/faceStaticController.js
+++ HealthChecklist/controllers/faceStaticController.js
@@ -7,17 +7,14 @@
     function FaceStaticController(OrderCreateService, $location, $scope) {
         if (OrderCreateService.dataCache.faceStatic != undefined) {
             var faceStatic = OrderCreateService.dataCache.faceStatic;
-            $scope.order_faceStatic_ztmxs = faceStatic.order_faceStatic_ztmxs;
-            $scope.order_faceStatic_ztmxm = faceStatic.order_faceStatic_ztmxm;
-            $scope.order_faceStatic_ztmxl = faceStatic.order_faceStatic_ztmxl;
+            $scope.order_faceStatic_ztmx = faceStatic.order_faceStatic_ztmx;
             $scope.order_faceStatic_xmbgd = faceStatic.order_faceStatic_xmbgd;
             $scope.order_faceStatic_rzgd = faceStatic.order_faceStatic_rzgd;
             $scope.order_faceStatic_kjgdz = faceStatic.order_faceStatic_kjgdz;
             $scope.order_faceStatic_kjgdy = faceStatic.order_faceStatic_kjgdy;
             $scope.order_faceStatic_mxczbls = faceStatic.order_faceStatic_mxczbls;
             $scope.order_faceStatic_mxczblx = faceStatic.order_faceStatic_mxczblx;
-            $scope.order_faceStatic_cwfbha = faceStatic.order_faceStatic_cwfbha;
-            $scope.order_faceStatic_cwfbhb = faceStatic.order_faceStatic_cwfbhb;
+            $scope.order_faceStatic_cwfbh = faceStatic.order_faceStatic_cwfbh;
             $scope.order_faceStatic_ncjj = faceStatic.order_faceStatic_ncjj;
             $scope.order_faceStatic_ykd = faceStatic.order_faceStatic_ykd;
             $scope.order_faceStatic_byjd = faceStatic.order_faceStatic_byjd;
@@ -31,22 +28,14 @@
             $scope.order_faceStatic_seyghzx = faceStatic.order_faceStatic_seyghzx;
             $scope.order_faceStatic_xhdcx = faceStatic.order_faceStatic_xhdcx;
             $scope.order_faceStatic_xeyghzzlh = faceStatic.order_faceStatic_xeyghzzlh;
-            $scope.order_faceStatic_xedcxa = faceStatic.order_faceStatic_xedcxa;
-            $scope.order_faceStatic_xedcxb = faceStatic.order_faceStatic_xedcxb;
-            $scope.order_faceStatic_hdcxa = faceStatic.order_faceStatic_hdcxa;
-            $scope.order_faceStatic_hdcxb = faceStatic.order_faceStatic_hdcxb;
+            $scope.order_faceStatic_xedcx = faceStatic.order_faceStatic_xedcx;
+            $scope.order_faceStatic_hdcx = faceStatic.order_faceStatic_hdcx;
             $scope.order_faceStatic_zzlhhszmcy = faceStatic.order_faceStatic_zzlhhszmcy;
             $scope.order_faceStatic_zchpgd = faceStatic.order_faceStatic_zchpgd;
             $scope.order_faceStatic_ychpgd = faceStatic.order_faceStatic_ychpgd;
-            $scope.order_faceStatic_sepma = faceStatic.order_faceStatic_sepma;
-            $scope.order_faceStatic_sepmb = faceStatic.order_faceStatic_sepmb;
-            $scope.order_faceStatic_bkdcxa = faceStatic.order_faceStatic_bkdcxa;
-            $scope.order_faceStatic_bkdcxb = faceStatic.order_faceStatic_bkdcxb;
-            $scope.order_faceStatic_bkdcxc = faceStatic.order_faceStatic_bkdcxc;
-            $scope.order_faceStatic_bzgpqa = faceStatic.order_faceStatic_bzgpqa;
-            $scope.order_faceStatic_bzgpqb = faceStatic.order_faceStatic_bzgpqb;
-            $scope.order_faceStatic_bzgpqc = faceStatic.order_faceStatic_bzgpqc;
-            $scope.order_faceStatic_xbjfda = faceStatic.order_faceStatic_xbjfda;
+            $scope.order_faceStatic_sepm = faceStatic.order_faceStatic_sepm;
+            $scope.order_faceStatic_bkdcx = faceStatic.order_faceStatic_bkdcx;
+            $scope.order_faceStatic_bzgpq = faceStatic.order_faceStatic_bzgpq;
             $scope.order_faceStatic_xbjfdb = faceStatic.order_faceStatic_xbjfdb;
 
         }
@@ -56,17 +45,14 @@
                 OrderCreateService.dataCache.faceStatic = {};
             }
             var faceStatic = OrderCreateService.dataCache.faceStatic;
-            faceStatic.order_faceStatic_ztmxs = $scope.order_faceStatic_ztmxs;
-            faceStatic.order_faceStatic_ztmxm = $scope.order_faceStatic_ztmxm;
-            faceStatic.order_faceStatic_ztmxl = $scope.order_faceStatic_ztmxl;
+            faceStatic.order_faceStatic_ztmx = $scope.order_faceStatic_ztmx;
             faceStatic.order_faceStatic_xmbgd = $scope.order_faceStatic_xmbgd;
             faceStatic.order_faceStatic_rzgd = $scope.order_faceStatic_rzgd;
             faceStatic.order_faceStatic_kjgdz = $scope.order_faceStatic_kjgdz;
             faceStatic.order_faceStatic_kjgdy = $scope.order_faceStatic_kjgdy;
             faceStatic.order_faceStatic_mxczbls = $scope.order_faceStatic_mxczbls;
             faceStatic.order_faceStatic_mxczblx = $scope.order_faceStatic_mxczblx;
-            faceStatic.order_faceStatic_cwfbha = $scope.order_faceStatic_cwfbha;
-            faceStatic.order_faceStatic_cwfbhb = $scope.order_faceStatic_cwfbhb;
+            faceStatic.order_faceStatic_cwfbh = $scope.order_faceStatic_cwfbh;
             faceStatic.order_faceStatic_ncjj = $scope.order_faceStatic_ncjj;
             faceStatic.order_faceStatic_ykd = $scope.order_faceStatic_ykd;
             faceStatic.order_faceStatic_byjd = $scope.order_faceStatic_byjd;
@@ -80,22 +66,14 @@
             faceStatic.order_faceStatic_seyghzx = $scope.order_faceStatic_seyghzx;
             faceStatic.order_faceStatic_xhdcx = $scope.order_faceStatic_xhdcx;
             faceStatic.order_faceStatic_xeyghzzlh = $scope.order_faceStatic_xeyghzzlh;
-            faceStatic.order_faceStatic_xedcxa = $scope.order_faceStatic_xedcxa;
-            faceStatic.order_faceStatic_xedcxb = $scope.order_faceStatic_xedcxb;
-            faceStatic.order_faceStatic_hdcxa = $scope.order_faceStatic_hdcxa;
-            faceStatic.order_faceStatic_hdcxb = $scope.order_faceStatic_hdcxb;
+            faceStatic.order_faceStatic_xedcx = $scope.order_faceStatic_xedcx;
+            faceStatic.order_faceStatic_hdcx = $scope.order_faceStatic_hdcx;
             faceStatic.order_faceStatic_zzlhhszmcy = $scope.order_faceStatic_zzlhhszmcy;
             faceStatic.order_faceStatic_zchpgd = $scope.order_faceStatic_zchpgd;
             faceStatic.order_faceStatic_ychpgd = $scope.order_faceStatic_ychpgd;
-            faceStatic.order_faceStatic_sepma = $scope.order_faceStatic_sepma;
-            faceStatic.order_faceStatic_sepmb = $scope.order_faceStatic_sepmb;
-            faceStatic.order_faceStatic_bkdcxa = $scope.order_faceStatic_bkdcxa;
-            faceStatic.order_faceStatic_bkdcxb = $scope.order_faceStatic_bkdcxb;
-            faceStatic.order_faceStatic_bkdcxc = $scope.order_faceStatic_bkdcxc;
-            faceStatic.order_faceStatic_bzgpqa = $scope.order_faceStatic_bzgpqa;
-            faceStatic.order_faceStatic_bzgpqb = $scope.order_faceStatic_bzgpqb;
-            faceStatic.order_faceStatic_bzgpqc = $scope.order_faceStatic_bzgpqc;
-            faceStatic.order_faceStatic_xbjfda = $scope.order_faceStatic_xbjfda;
+            faceStatic.order_faceStatic_sepm = $scope.order_faceStatic_sepm;
+            faceStatic.order_faceStatic_bkdcx = $scope.order_faceStatic_bkdcx;
+            faceStatic.order_faceStatic_bzgpq = $scope.order_faceStatic_bzgpq;
             faceStatic.order_faceStatic_xbjfdb = $scope.order_faceStatic_xbjfdb;
 
         };
